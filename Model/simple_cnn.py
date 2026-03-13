3929b0741f58731c76f651187d4ff78bef933556
MahmoudSayed216 <mahmoud_das@outlook.com>
2026-03-13 20:15:13 +0200

    sixth test

--- Model/simple_cnn.py
+++ Model/simple_cnn.py
@@ -1,5 +1,5 @@
+import torch
 import torch.nn as nn
-
 
 class SimpleCNN(nn.Module):
     def __init__(self):
@@ -7,26 +7,39 @@ class SimpleCNN(nn.Module):
 
         self.features = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2),
 
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2),
 
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.MaxPool2d(2)
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
         )
 
+        # Adaptive average pooling to 1x1 -> makes it input-size agnostic
+        self.global_avg_pool = nn.AdaptiveAvgPool2d(1)
+
         self.classifier = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(128 * 28 * 28, 128),  # assumes input 224x224
+            nn.Flatten(),         # will just be [batch_size, 256]
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
             nn.ReLU(),
+            nn.Dropout(0.5),
             nn.Linear(128, 1)
         )
 
     def forward(self, x):
         x = self.features(x)
+        x = self.global_avg_pool(x)   # [batch_size, 256, 1, 1]
         x = self.classifier(x)
         return x
